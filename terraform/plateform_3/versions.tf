0e08567ab3fad9b3134a46bc425c154fce8cf26c
Yanis Chaabane <yanis.chaabane@epita.fr>
2022-11-15 01:29:34 +0100

    wip(terraform): adding sample for api gateway

--- terraform/plateform_3/versions.tf
+++ terraform/plateform_3/versions.tf
@@ -4,6 +4,14 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 4.7"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 2.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 2.0"
+    }
   }
 
   required_version = ">= 1.2.0"
