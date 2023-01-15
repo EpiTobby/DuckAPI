b68d1c6d788e6ad2ae31864f6c4577b47ef42e02
Gabriel Rayzal <gabriel.rayzal@epita.fr>
2023-01-15 14:20:17 +0100

    platform 3 (feat): deploy lambda get_all & get_by_id

--- terraform/plateform_3/lambda.tf
+++ terraform/plateform_3/lambda.tf
@@ -4,3 +4,17 @@ module "lambda_create" {
   iam_role_arn = aws_iam_role.lambdas.arn
   name         = "create_duck"
 }
+
+module "lambda_get_all" {
+  source = "./modules/duck_lambda"
+
+  iam_role_arn = aws_iam_role.lambdas.arn
+  name         = "get_all_ducks"
+}
+
+module "lambda_get_by_id" {
+  source = "./modules/duck_lambda"
+
+  iam_role_arn = aws_iam_role.lambdas.arn
+  name         = "get_duck_by_uuid"
+}
