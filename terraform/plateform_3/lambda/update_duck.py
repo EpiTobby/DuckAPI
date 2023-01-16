e811d5e882d093e3830a4cb41f11cbcc2d07e6b2
gabrielbouhnik <49732226+gabrielbouhnik@users.noreply.github.com>
2023-01-16 18:16:19 +0100

    add lambda to update duck

---
+++ terraform/plateform_3/lambda/update_duck.py
@@ -0,0 +1,50 @@
+import json
+import decimal
+import boto3
+
+
+def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+
+    table = dynamodb.Table('ducks')
+
+    queryParameters = event['params']['querystring']
+    if 'uuid' in queryParameters:
+        uuid = queryParameters['uuid']
+        if 'age' in event["body"]:
+            resp = table.update_item(
+                 Key={'uuid': uuid},
+                 UpdateExpression="SET age= :s",
+                 ExpressionAttributeValues={':s': event["body"]["age"]},
+                 ReturnValues="UPDATED_NEW"
+            )
+            print(resp['Attributes'])
+        if 'color' in event["body"]:
+            resp = table.update_item(
+                 Key={'uuid': uuid},
+                 UpdateExpression="SET color= :s",
+                 ExpressionAttributeValues={':s': event["body"]["color"].upper()},
+                 ReturnValues="UPDATED_NEW"
+            )
+            print(resp['Attributes'])
+        resp = table.get_item(
+            Key={
+                'uuid': uuid
+            },
+        )
+        return {
+            'statusCode': 200,
+            'body': json.dumps(resp['Item'], default=to_serializable)
+        }
+    return {
+        'statusCode': 404,
+        'body': json.dumps({})
+    }
+
+def to_serializable(val):
+    """JSON serializer for objects not serializable by default"""
+
+    if type(val) is decimal.Decimal:
+        return int(val)
+
+    return val
