09e7eabe08c8a4825d484c5d8f5d07285f5b243a
Gabriel Rayzal <gabriel.rayzal@epita.fr>
2023-01-17 12:57:54 +0100

    platform 3 (feat): deploy frontend in a bucket s3

---
+++ terraform/plateform_3/front.tf
@@ -0,0 +1,72 @@
+resource "aws_s3_bucket" "front" {
+  bucket        = "www.epita-arinf-duckapi-bucket"
+  force_destroy = true
+
+}
+
+resource "aws_s3_bucket_acl" "front" {
+  bucket = aws_s3_bucket.front.bucket
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "front" {
+  bucket = aws_s3_bucket.front.bucket
+  policy = data.aws_iam_policy_document.public_read.json
+}
+
+data "aws_iam_policy_document" "public_read" {
+  statement {
+    principals {
+      identifiers = ["*"]
+      type        = "*"
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.front.arn,
+      "${aws_s3_bucket.front.arn}/*",
+    ]
+  }
+}
+
+resource "aws_s3_object" "front_build" {
+  # for_each = fileset("../../duckfront/build", "**")
+  for_each     = module.dir.files
+  key          = each.key
+  content_type = each.value.content_type
+
+  bucket        = aws_s3_bucket.front.bucket
+  source        = each.value.source_path
+  force_destroy = true
+
+  etag = each.value.digests.md5
+}
+
+module "dir" {
+  source   = "hashicorp/dir/template"
+  version  = "1.0.2"
+  # insert the 1 required variable here
+  base_dir = "../../duckfront/build/"
+}
+
+resource "aws_s3_bucket_website_configuration" "front" {
+  bucket = aws_s3_bucket.front.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  depends_on = [aws_s3_object.front_build]
+}
+
+output "website_domain" {
+  value = aws_s3_bucket_website_configuration.front.website_domain
+}
+
+output "website_endpoint" {
+  value = aws_s3_bucket_website_configuration.front.website_endpoint
+}
